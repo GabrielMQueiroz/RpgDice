00fbceb8fdd2625f369577f2f30f43b2b81b5d15
Gabriel Mendonça de Queiroz <gabrielqueiroz167@gmail.com>
2023-02-23 00:33:11 -0300

    funcionou

--- script.js
+++ script.js
@@ -1,39 +1,22 @@
 let VD = 0
 let buffer ="0"
 //let previousDice
+function roll(){
+    VD=getRndInteger(1,30);
+    alert(VD)
+}
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
 
-const screen= document.querySelector('.screen');
+//const screen= document.querySelector('.screen');
 function buttonClick(value){
     if(isNaN(value)){
-        handleSymbol(value)
+        VD=roll
     }
     screen.innerText = buffer;
 }
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
-}
-function handleSymbol(symbol){
-    switch(symbol){
-        case  'Roll':
-            VD=getRndInteger(1,30);//(Math.floor(Math.random() * 30)+1)
-            buffer=parseStr((VD))
-            alert(VD)
-            break;
-        case 'Type':
-            alert('Esppiritum')
-    }
-}
-/*function handleMath(symbol){
-    if (buffer==='0'){
-        return;
-    }
-    const intBuffer = parseInt(buffer);
-    if(VD===0){
-        VD = intBuffer;
-    }else{
-        flushOperation(intBuffer)
-    }
-}*/
+
 function init(){
     document.querySelector('.Roll').addEventListener('click',function(event){
         buttonClick(event.target.innerText);        
