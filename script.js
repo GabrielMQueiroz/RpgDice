4987d9840c7cfddd0b8c3a5065bc05c5c1481e79
Gabriel Mendonça de Queiroz <gabrielqueiroz167@gmail.com>
2023-02-23 01:16:17 -0300

    Fixing display

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ let buffer ="0"
 //let previousDice
 function roll(){
     VD=getRndInteger(1,30);
+    //document.getElementById("Cock").innerHTML = "" + count + "";
+    document.getElementById("Cock").value = VD;
     alert(VD)
 }
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
   }
-
 //const screen= document.querySelector('.screen');
 function buttonClick(value){
     if(isNaN(value)){
@@ -16,10 +17,11 @@ function buttonClick(value){
     }
     screen.innerText = buffer;
 }
-
+/*
 function init(){
     document.querySelector('.Roll').addEventListener('click',function(event){
         buttonClick(event.target.innerText);        
     })
 }
 init();
+*/
